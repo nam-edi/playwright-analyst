60c13bba0ebc8afa3f3d52b74ead2a9271b6fa9f
nam-edi <hoffmann.dam@gmail.com>
2025-09-12 15:16:56 +0200

    security: Fix mark_safe vulnerabilities in ColorPickerWidge

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
+
+    def ready(self):
+        """Importe les vues d'admin personnalisées au démarrage"""
+        from . import admin_views  # noqa: F401
